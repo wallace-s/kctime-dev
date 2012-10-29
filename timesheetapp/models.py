c4e9de703c56e9ab55b8234527f72f5404388b49
Shawn Wallace <shawn.wallace@hotmail.com>
2012-10-29 19:03:45 +0900

    recovered repository
    the system time on the pc I was working on was set to 5 months in the
    future. this seemed to cause bugs with the git repository when I synced
    it.
    this version has the Person class removed but there some code that needs
    to be adjusted to get it working right again.

---
+++ timesheetapp/models.py
@@ -0,0 +1,52 @@
+﻿from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+import datetime
+
+MONTH_CHOICES = (
+    (datetime.date(year=2012, month=1, day=1),"2012年1月"),
+    (datetime.date(year=2012, month=2, day=1),"2012年2月"),
+    (datetime.date(year=2012, month=3, day=1),"2012年3月"),
+    (datetime.date(year=2012, month=4, day=1),"2012年4月"),
+    (datetime.date(year=2012, month=5, day=1),"2012年5月"),
+    (datetime.date(year=2012, month=6, day=1),"2012年6月"),
+    (datetime.date(year=2012, month=7, day=1),"2012年7月"),
+    (datetime.date(year=2012, month=8, day=1),"2012年8月"),
+    (datetime.date(year=2012, month=9, day=1),"2012年9月"),
+    (datetime.date(year=2012, month=10, day=1),"2012年10月"),
+    (datetime.date(year=2012, month=11, day=1),"2012年11月"),
+    (datetime.date(year=2012, month=12, day=1),"2012年12月"),
+)
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+
+
+class Timesheet(models.Model):
+        sheet_month = models.DateField(choices=MONTH_CHOICES)
+        user = models.ForeignKey(User)
+
+        def __unicode__(self):
+               return u"time sheet for %s for %s年 %s月" % (self.user.username, self.sheet_month.year, self.sheet_month.month)
+        
+        class Meta:
+            unique_together = (("user", "sheet_month"),)
+
+class TimeDuration(models.Model):
+        start_time = models.TimeField(help_text="テスティングのヘルプのテキスト")
+        end_time = models.TimeField()
+        paid_time_boolean = models.BooleanField(default="TRUE")
+        timesheet = models.ForeignKey(Timesheet)
+
+        def __unicode__(self):
+                return str(self.start_time) + " to " + str(self.end_time)
+
+        # def timediff(self):
+                # return datetime.timedelta(end_time, start_time)
+        #add a method here to return the duration between the two times
+
+    
---
+++ timesheetapp/views.py
@@ -0,0 +1,31 @@
+﻿from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext 
+from timesheetapp.models import Timesheet
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+@login_required
+def index(request):
+	return render_to_response('timesheetapp/index.html', {}, context_instance=RequestContext(request))
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/login/")
+	
+def register(request):
+    if request.method == "POST":
+        userform = UserCreationForm(request.POST)
+        if userform.is_valid():
+            userform.save()
+            return HttpResponse("it worked!")
+        else:
+            #render_to_response('timesheetapp/register.html', {"The form is invalid":errormsg},context_instance=RequestContext(request))
+			return HttpResponse("the bug is here")
+    else:
+        userform = UserCreationForm()
+        return render_to_response('timesheetapp/register.html', {"userform":userform},context_instance=RequestContext(request))
+
+def login(request):
+    return render_to_response('timesheetapp/login.html')
